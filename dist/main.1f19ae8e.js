e2b26f034eae2b88f9d9fa2ced3ef65cab99eaf1
903958734@qq.com <903958734@qq.com>
2020-09-01 21:19:08 +0800

    dd

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -143,7 +143,7 @@ var step = function step() {
       html.scrollTo(0, 99999);
       step();
     }
-  }, 5);
+  }, 50);
 };
 
 step();
@@ -175,7 +175,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58636" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64039" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
